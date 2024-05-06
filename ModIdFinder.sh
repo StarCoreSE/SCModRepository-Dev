2b595c17c5d9a806a9c9f65791cb486f0132104b
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-05-06 15:22:50 -0500

    Create ModIdFinder.sh

---
+++ ModIdFinder.sh
@@ -0,0 +1,17 @@
+find . -type f -name "*.sbmi" >> ./allModDatas.txt
+
+MODIDARR=()
+
+while read path; do
+  echo Reading $path
+  while read sbmiLine; do
+      if [[ $sbmiLine = \<Id\>* ]] ; then
+          tmp=${sbmiLine#*>}
+          modId=${tmp%<*}
+          MODIDARR+=($modId)
+      fi
+  done < "$path"
+done < allModDatas.txt
+
+data_string="${MODIDARR[*]}"
+echo "[${data_string//${IFS:0:1}/,}]"
